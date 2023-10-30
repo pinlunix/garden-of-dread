ec453ca26a6cf5a5329fb01bd6032284de57df91
pinlunix <pheonlu@gmail.com>
2023-10-30 03:27:19 -0700

    Player can Jump and Jump-Cancel
    Player can now jump and jump-cancel by pressing jump button while mid-jump. Created physics material for the player object.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,27 @@ using System.Diagnostics;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
 using UnityEngine.InputSystem;
+using System.Security.Cryptography;
+using System.Collections.Specialized;
 
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
-
+    [Header("Horizontal Movement")]
     public float walkSpeed = 10;
     Vector2 moveDir;
-    Rigidbody2D rb;
     private float movementX;
     private float movementY;
 
-    [SerializeField] private float jumpForce = 45;
+    [SerializeField] private float jumpForce = 45f;
+
+    [Header("Ground Check")]
+    [SerializeField] private Transform groundCheckPoint;    // use as origin
+    [SerializeField] private float groundCheckY = 0.2f;     // define distance of ray
+    [SerializeField] private float groundCheckX = 0.5f;     // check if player is standing near an edge of platform
+    [SerializeField] private LayerMask whatIsGround;        // detect game objects on ground layer
+
+    Rigidbody2D rb;
 
     void Start()
     {
@@ -23,7 +32,19 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void OnMove(InputValue movementValue) {
+    private void FixedUpdate()
+    {
+        //int jump_scalar = 0;
+        //if (gameObject.transform.position.y == 1)
+        //{
+        //    jump_scalar = 30;
+        //}
+        Vector2 movement = new Vector2(movementX * 10, movementY * 30);
+        rb.AddForce(movement);
+    }
+
+    void OnMove(InputValue movementValue)
+    {
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x;
         movementY = movementVector.y;
@@ -31,17 +52,31 @@ public class PlayerMovement : MonoBehaviour
         Debug.Log("moving");
     }
 
-
-    private void FixedUpdate()
+    void OnJump()
     {
-        int jump_scalar = 0;
-        if (gameObject.transform.position.y == 1)
+        if (Grounded())
         {
-            jump_scalar = 30;
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce);
+            Debug.Log("Jumped");
         }
-        Vector2 movement = new Vector2(movementX * 10, movementY * 30);
-        rb.AddForce(movement);
+        if (!Grounded() && rb.velocity.y > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+        }
     }
 
+    public bool Grounded()
+    {
+        if (Physics2D.Raycast(groundCheckPoint.position, Vector2.down, groundCheckY, whatIsGround)
+            || Physics2D.Raycast(groundCheckPoint.position + new Vector3(groundCheckX, 0, 0), Vector2.down, groundCheckY, whatIsGround)
+            || Physics2D.Raycast(groundCheckPoint.position + new Vector3(-groundCheckX, 0, 0), Vector2.down, groundCheckY, whatIsGround))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
     
 }
