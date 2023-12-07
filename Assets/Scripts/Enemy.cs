60527e185c44c45fea9cecd9f141e056deaa9e17
pinlunix <pheonlu@gmail.com>
2023-12-07 05:53:45 -0800

    Health bar UI, update enemy scripts, update tilemap collider

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
+using UnityEngine.InputSystem;
 
 public class Enemy : MonoBehaviour
 {
@@ -19,33 +20,55 @@ public class Enemy : MonoBehaviour
     protected float recoilTimer;
     protected Rigidbody2D rb;
     private Animator anim;
+    protected SpriteRenderer sr;
+    protected private float resetAttackTimer;
 
     protected enum EnemyStates
     {
         // GreenMob
         GreenMob_Idle,
-        GreenMob_Flip
+        GreenMob_Flip,
+
+        // FlyingMob
+        FlyingMob_Idle,
+        FlyingMob_Chase,
+        FlyingMob_Stunned,
+        FlyingMob_Death
     }
 
     protected EnemyStates currentEnemyState;
+
+    protected virtual EnemyStates GetCurrentEnemyState{
+        get {
+            return currentEnemyState;
+        }
+        set{
+            if(currentEnemyState != value){
+                currentEnemyState = value;
+                ChangeCurrentAnimation();
+            }
+        }
+    }
 
     // Start is called before the first frame update
     protected virtual void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+
+        resetAttackTimer = 0;
     }
 
     // Update is called once per frame
     protected virtual void Update()
     {
-        UpdateEnemyStates();
-
-        if(health <= 0)
-        {
-            Destroy(gameObject);
+        resetAttackTimer += Time.deltaTime;
+        if (resetAttackTimer > 3.0f && anim.GetBool("isAttacking")){
+            anim.ResetTrigger("isAttacking");
+            resetAttackTimer = 0;
         }
-
+        
         if (isRecoiling)
         {
             if (recoilTimer < recoilLength)
@@ -57,6 +80,9 @@ public class Enemy : MonoBehaviour
                 isRecoiling = false;
                 recoilTimer= 0;
             }
+        }
+        else{
+            UpdateEnemyStates();
         }
     }
 
@@ -75,8 +101,11 @@ public class Enemy : MonoBehaviour
         if(_other.CompareTag("Player") && !PlayerMovement.Instance.invincible)
         {            
             Attack();
-            anim.SetBool("isAttacking", true);
         }
+    }
+
+    protected virtual void Death(float _destroyTime){
+        Destroy(gameObject, _destroyTime);
     }
 
     protected virtual void UpdateEnemyStates()
@@ -84,14 +113,19 @@ public class Enemy : MonoBehaviour
 
     }
 
+    protected virtual void ChangeCurrentAnimation(){
+
+    }
+
     protected void ChangeState(EnemyStates _newState)
     {
-        currentEnemyState = _newState;
+        GetCurrentEnemyState = _newState;
     }
 
     protected virtual void Attack()
     {
         Vector2 knockback = collideDir * knockbackForce;
         PlayerMovement.Instance.TakeDamage(damage, knockback);
+        anim.SetTrigger("isAttacking");
     }
 }
